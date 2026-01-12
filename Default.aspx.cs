b2d7443c45387ac6c554fbd46ee995e80d911c58
Your Name <you@example.com>
2026-01-12 22:15:21 +0100

    Initial ASP.NET project with database connection

---
+++ Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace WebAplikacijaZaDostavuPice
+{
+    public partial class _Default : Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["PizzaDeliveryDBConnectionString"].ConnectionString;
+
+        protected void Page_Load( object sender, EventArgs e )
+        {
+
+        }
+    }
+}
